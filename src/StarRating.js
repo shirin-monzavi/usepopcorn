96c3b9f950f3c367fd58b5ead904c37e0c768b18
monzavi.shirin <monzavi.shirin@GPA001P2102485>
2026-02-22 13:59:24 +0330

    Complete rating component

--- src/StarRating.js
+++ src/StarRating.js
@@ -9,41 +9,52 @@ const starContainer = {
     display: "flex",
     gap: 4
 }
-const textStyle = {
-    lineHeight: 1,
-    margin: 0
-}
-const starStyle = {
-    width: 30,
-    height: 30,
-    display: "block"
-}
-export default function StarRating({ maxRate = 5 }) {
-    const [rate, setRate] = useState(0);
+
+export default function StarRating({ maxRate = 5, color = '#ffe121', size = 50, message = [], onSetRate, defualtRate = 0}) {
+    const textStyle = {
+        lineHeight: 1,
+        margin: 0,
+        color: color
+    }
+    const [rate, setRate] = useState(defualtRate);
     const [tempRate, setTempRate] = useState(0);
 
     function handleRate(index) {
         setRate(index);
+        onSetRate(index);
     }
     return (
         <div style={container}>
             <div style={starContainer}>
                 {Array.from({ length: maxRate }, (_, i) => {
-                    return <Star key={i}
+                    return <Star
+                        key={i}
                         onRate={() => handleRate(i + 1)}
                         full={tempRate ? tempRate >= i + 1 : rate >= i + 1}
                         onHoverIn={() => setTempRate(i + 1)}
                         onHoverOut={() => setTempRate(0)}
+                        color={color}
+                        size={size}
                     />
                 })}
             </div>
 
-            <p style={textStyle}>{rate || tempRate || ""}</p>
+            <p style={textStyle}>{message.length > 0 ? message[tempRate ? (tempRate - 1) : (rate - 1)] : rate || tempRate || ""}</p>
+
+
         </div>
     )
 }
 
-function Star({ onRate, full, onHoverIn, onHoverOut }) {
+
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
+    const starStyle = {
+        width: size,
+        display: "block",
+        color: color,
+        fontSize: `${size}px`
+    }
+
     return (
 
         (<span style={starStyle} role="button"
@@ -53,8 +64,8 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
             {full ? <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
-                fill="#000"
-                stroke="#000"
+                fill={color}
+                stroke={color}
             >
                 <path
                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -63,7 +74,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut }) {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke="#000"
+                stroke={color}
             >
                 <path
                     strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -4,15 +4,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import StarRating from './StarRating';
+import { useState } from "react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRate={10} />
+    <StarRating maxRate={3} color='red' size={50} message={['bad', 'ok', 'good']} defualtRate={2} />
+    <Test />
   </React.StrictMode>
 );
 
+function Test() {
+    const [movie, setMovieRate] = useState(0);
+    return (
+        <>
+            <StarRating maxRate={3} color='red' size={50} message={['bad', 'ok', 'good']} onSetRate ={setMovieRate} />
+            <p>Rate is: {movie}</p>
+        </>
+    )
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
